52b2b4c509571df7f8abe251064ce0eee4ddb80b
Carlos Power <powercarlos25@fb.com>
2019-06-27 10:46:35 -0700

    Milestone #2 completed

---
+++ Tumblr/PhotosViewController.h
@@ -0,0 +1,17 @@
+//
+//  PhotosViewController.h
+//  Tumblr
+//
+//  Created by powercarlos25 on 6/27/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotosViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tumblr/PhotosViewController.m
@@ -0,0 +1,59 @@
+//
+//  PhotosViewController.m
+//  Tumblr
+//
+//  Created by powercarlos25 on 6/27/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import "PhotosViewController.h"
+
+@interface PhotosViewController ()
+
+@property (strong, nonatomic) NSArray *posts;
+
+@end
+
+@implementation PhotosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        else {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+            // TODO: Get the posts and store in posts property
+            
+            // Get the dictionary from the response key
+            NSDictionary *responseDictionary = dataDictionary[@"response"];
+            // Store the returned array of dictionaries in our posts property
+            self.posts = responseDictionary[@"posts"];
+            
+            //NSLog(@"%@", self.posts);
+            //NSLog(@"%@", dataDictionary);
+
+            // TODO: Reload the table view
+        }
+    }];
+    [task resume];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
